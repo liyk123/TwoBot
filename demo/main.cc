02242f0ca6542babc021c247815b223655ff5ad0
liyk123 <liyk__@outlook.com>
2024-05-21 18:04:22 +0800

    更新demo

--- demo/main.cc
+++ demo/main.cc
@@ -8,48 +8,64 @@ using twobot::Config;
 using twobot::BotInstance;
 using twobot::ApiSet;
 using namespace twobot::Event;
+using twobot::Session;
 
 int main(int argc, char** args) {
     // 解决UTF8编码，中文乱码问题，不需要可以不加
 #ifdef _WIN32
     system("chcp 65001 && cls");
 #endif
-    auto instance = BotInstance::createInstance(Config{
-        "0.0.0.0",
-        0,
+    Config config = {
+        "10.8.0.1",
+        5700,
         9444,
-        std::nullopt
+        ""
+    };
+    auto instance = BotInstance::createInstance(config);
+
+
+    if(!instance->getApiSet().testConnection()){
+        std::cerr << "测试连接失败，请启动onebot服务器，并配置HTTP端口！" << std::endl;
+    }
+
+    instance->onEvent<GroupMsg>([&instance](const GroupMsg & msg, const Session::Ptr& session){
+        twobot::ApiSet::ApiResult r = {};
+        if (msg.raw_message == "你好") 
+        {
+            r = instance->getApiSet().sendGroupMsg(msg.group_id, "你好，我是twobot！");
+        }
+		else if (msg.raw_message.find("AT我") != std::string::npos) 
+        {
+			std::string at = "[CQ:at,qq=" + std::to_string(msg.user_id) + "]";
+			r = instance->getApiSet().sendGroupMsg(msg.group_id, at + "要我at你干啥？");
+        }
+        else if (msg.raw_message == "getGroupMemberList") 
+        {
+            r = instance->getApiSet(session).getGroupMemberList(msg.group_id);
+            instance->getApiSet().sendGroupMsg(msg.group_id, "获取成功");
+        }
+        std::cout << r.second.dump(4) << std::endl;
     });
 
-
-    //if(!instance->getApiSet().testConnection()){
-    //    std::cerr << "测试连接失败，请启动onebot服务器，并配置HTTP端口！" << std::endl;
-    //}
-
-    instance->onEvent<GroupMsg>([&instance](const GroupMsg & msg){
-        if(msg.raw_message == "你好")
-            instance->getApiSet().sendGroupMsg(msg.group_id, "你好，我是twobot！");
-        else if(msg.raw_message.find("AT我") != std::string::npos){
-            std::string at = "[CQ:at,qq=" + std::to_string(msg.user_id) + "]";
-            instance->getApiSet().sendGroupMsg(msg.group_id, at + "要我at你干啥？");
+    instance->onEvent<PrivateMsg>([&instance](const PrivateMsg & msg, const Session::Ptr& session){
+        if (msg.raw_message == "你好")
+        {
+            ApiSet set = instance->getApiSet(session);
+            auto &r = set.sendPrivateMsg(msg.user_id, "你好，我是twobot！");
+            std::cout << r << std::endl;
         }
     });
 
-    instance->onEvent<PrivateMsg>([&instance](const PrivateMsg & msg){
-        if(msg.raw_message == "你好")
-            instance->getApiSet().sendPrivateMsg(msg.user_id, "你好，我是twobot！");
-        
-    });
 
-    instance->onEvent<EnableEvent>([&instance](const EnableEvent & msg){
+    instance->onEvent<EnableEvent>([&instance](const EnableEvent & msg, const Session::Ptr& session){
         std::cout << "twobot已启动！机器人QQ："<< msg.self_id << std::endl;
     });
 
-    instance->onEvent<DisableEvent>([&instance](const DisableEvent & msg){
+    instance->onEvent<DisableEvent>([&instance](const DisableEvent & msg, const Session::Ptr& session){
         std::cout << "twobot已停止！" << std::endl;
     });
 
-    instance->onEvent<ConnectEvent>([&instance](const ConnectEvent & msg){
+    instance->onEvent<ConnectEvent>([&instance](const ConnectEvent & msg, const Session::Ptr& session){
         std::cout << "twobot已连接！" << std::endl;
     });
 
