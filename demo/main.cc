fabfebf1f61fa14e4a9ab3a7b0756d9cce034667
liyk123 <liyk__@outlook.com>
2024-08-12 16:24:40 +0800

    tmp

--- demo/main.cc
+++ demo/main.cc
@@ -1,6 +1,5 @@
 ﻿#include <twobot.hh>
 #include <iostream>
-#include <httplib.h>
 
 /// 警告: 这个项目使用了JSON for modern C++, 必须使用UTF8编码，不然会出现异常。
 /// warning: this project uses JSON for modern C++, must use UTF8 encoding, otherwise it will throw exception.
@@ -10,27 +9,6 @@ using twobot::BotInstance;
 using twobot::ApiSet;
 using namespace twobot::Event;
 using twobot::Session;
-
-static inline std::string getRealID(uint64_t vid)
-{
-	std::string realID = "";
-    std::stringstream ss;
-    ss << vid;
-    nlohmann::json data = {
-        { "type", "2"},
-        { "id", ss.str()}
-    };
-    httplib::Client client("http://10.8.0.1:9444");
-    auto r = client.Get("/getid", httplib::Params(data), {});
-    //const auto& r = instance->getApiSet(false).callApi("/getid", data);
-    if (r != nullptr)
-    {
-        auto ret = nlohmann::json::parse(r->body);
-        std::cout << ret.dump() << std::endl;
-        realID = ret.value("id", realID);
-    }
-    return realID;
-}
 
 int main(int argc, char** args) {
     // 解决UTF8编码，中文乱码问题，不需要可以不加
@@ -65,34 +43,36 @@ int main(int argc, char** args) {
 			std::string at = "[CQ:at,qq=" + std::to_string(msg.user_id) + "]";
 			r = instance->getApiSet().sendGroupMsg(msg.group_id, at + "要我at你干啥？");
         }
-        else if (msg.raw_message == "getGroupMemberList") 
+        else if (msg.raw_message == "头像")
         {
-            r = instance->getApiSet(session).getGroupMemberList(msg.group_id);
-            instance->getApiSet().sendGroupMsg(msg.group_id, "获取成功");
+			char buf[64];
+			std::sprintf(buf, "[CQ:avatar,qq=%llu]", msg.user_id);
+			r = instance->getApiSet().sendGroupMsg(msg.group_id, std::string(buf));
         }
-        else if (msg.raw_message == "getAvatar")
+        else if (msg.raw_message == "私聊")
         {
-            std::string group_id = getRealID(msg.group_id);
-            std::string user_id = getRealID(msg.user_id);
-			nlohmann::json param = {
-                {"group_id", "B6240B4B49D1EAC79F423C7C39D5D460"},
-                {"user_id", "CCBEAF4DD759BE6C3580672243972A69"}
-            };
-            r = instance->getApiSet().callApi("/get_avatar", param);
-			if (r.second != nullptr) {
-                instance->getApiSet().sendGroupMsg(msg.group_id, r.second.dump(4));
-            }
+            r = instance->getApiSet().sendPrivateMsg(msg.user_id, "你好，我是twobot！");
         }
-        std::cout << r.second.dump(4) << std::endl;
+
+        std::cout << r.second.dump() << std::endl;
     });
 
     instance->onEvent<PrivateMsg>([&instance](const PrivateMsg & msg, const Session::Ptr& session){
+        twobot::ApiSet::ApiResult r = {};
         if (msg.raw_message == "你好")
         {
-            ApiSet set = instance->getApiSet(session);
-            const auto &r = set.sendPrivateMsg(msg.user_id, "你好，我是twobot！");
-            std::cout << r << std::endl;
+            r = instance->getApiSet(session).sendPrivateMsg(msg.user_id, "你好，我是twobot！");
         }
+        else if (msg.raw_message == "头像")
+        {
+            nlohmann::json param = {
+                {"user_id", msg.user_id}
+            };
+			char buf[64];
+			std::sprintf(buf, "[CQ:avatar,qq=%llu]", msg.user_id);
+			r = instance->getApiSet(session).sendPrivateMsg(msg.user_id, std::string(buf));
+        }
+        std::cout << r.second.dump() << std::endl;
     });
 
 
@@ -101,11 +81,11 @@ int main(int argc, char** args) {
     });
 
     instance->onEvent<DisableEvent>([&instance](const DisableEvent & msg, const Session::Ptr& session){
-        std::cout << "twobot已停止！" << std::endl;
+        std::cout << "twobot已停止！ID: " << msg.self_id << std::endl;
     });
 
     instance->onEvent<ConnectEvent>([&instance](const ConnectEvent & msg, const Session::Ptr& session){
-        std::cout << "twobot已连接！" << std::endl;
+        std::cout << "twobot已连接！ID: " << msg.self_id << std::endl;
     });
 
 
